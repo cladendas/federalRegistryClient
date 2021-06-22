5fff7e624541e9e208643e6bd8e6e77c494d1092
cladendas <rvdagi05@yandex.ru>
2021-06-22 10:19:50 +0300

    Initial Commit

---
+++ federalRegistryClient/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  federalRegistryClient
+//
+//  Created by cladendas on 22.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
